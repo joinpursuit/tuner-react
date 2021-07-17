2c8ab105bb35814b72484fe736772d6dd429cab0
Carlo <carlonyc001@gmail.com>
2021-07-17 15:45:46 -0400

    new song controller complete

--- tuner-react-frontend/src/Components/SongNewForm.js
+++ tuner-react-frontend/src/Components/SongNewForm.js
@@ -1,15 +1,82 @@
-import React from 'react'
-import axios from  'axios'
-import { useHistory } from 'react-router-dom'
-import { apiURAL } from '../util/apiURL'
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { apiURL } from "../util/apiURL";
+const API = apiURL();
 
 function SongNewForm() {
-  const
+  let history = useHistory();
+  const [song, setSong] = useState({
+    name: "",
+    album: "",
+    time: "",
+    is_favorite: false,
+  });
+
+  const addSong = async (newSong) => {
+    try {
+      await axios.post(`${API}/songs`, newSong);
+      history.push(`/songs`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSong({ ...song, [event.target.id]: event.target.value });
+  };
+
+  const handleCheckboxChange = () => {
+    setSong({ ...song, is_favorite: !song.is_favorite });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addSong(song);
+  };
+
   return (
     <div>
-      ADD NEW SONG
+      <h1>ADD NEW SONG</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          value={song.name}
+          type="text"
+          onChange={handleChange}
+          placeholder="Enter song name"
+        />
+        <label htmlFor="album">Album:</label>
+        <input
+          id="album"
+          value={song.album}
+          type="text"
+          onChange={handleChange}
+          placeholder="Enter album"
+        />
+        <label htmlFor="time">Time:</label>
+        <input
+          id="time"
+          value={song.time}
+          type="text"
+          onChange={handleChange}
+          placeholder="Enter time"
+        />
+        <label htmlFor="is_favorite">Favorite:</label>
+        <input
+          id="is_favorite"
+          value={song.is_favorite}
+          type="checkbox"
+          onChange={handleCheckboxChange}
+          checked={song.is_favorite}
+        />
+        <br />
+        <input type="submit" />
+      </form>
     </div>
-  )
+  );
 }
 
-export default SongNewForm
+export default SongNewForm;
--- tuner-react-frontend/src/Components/SongShow.js
+++ tuner-react-frontend/src/Components/SongShow.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { apiURL } from "../util/apiURL"
 import { Link } from "react-router-dom"
 
@@ -10,6 +10,8 @@ const API = apiURL();
 function SongShow() {
   const [song, setSong ] = useState("");
   const { id } = useParams();
+  let history = useHistory();
+
 useEffect(async () => {
   try {
     const res = await axios.get(`${API}/songs/${id}`)
@@ -19,13 +21,29 @@ useEffect(async () => {
   }
 })
 
+const deleteSong = async() => {
+  try {
+    await axios.delete(`${API}/songs/${id}`)
+  } catch (error) {
+    console.log(error)
+  }
+}
+const handleDelete = async () => {
+  await deleteSong();
+  history.push('/songs')
+}
+
   return (
     <div>
       <h1>The Song</h1>
-      {song.name}
+      <h1>Name: {song.name}</h1>
+      <h2>Album: {song.album}</h2>
+      <h3>Time: {song.time}</h3>
+      {/* <p>Favorite: {song.is_favorite}</p> */}
       <Link to={`/songs/edit/${id}`}>
         <button>Edit</button>
       </Link>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   )
 }
