8140ff525208fbff7d74c837b3033dbd1161663e
Talia Pichardo <taliapichardo@gmail.com>
2021-08-09 23:28:45 -0400

    Adds time to song card

--- tuner-front-end/src/Components/Song.js
+++ tuner-front-end/src/Components/Song.js
@@ -3,11 +3,10 @@ const Song = ({ song }) => {
         <div className="Card">
             <h4>{song.artist}</h4>
             {song.is_favorite === true ? <p><b>Favorite:</b> True</p> : <p><b>Favorite:</b> False</p>}
-            <p><b>Album: </b>{song.album}</p>
+            <p><b>Album: </b>{song.album} {song.time}</p>
             <p><b>Song: </b>{song.name}</p>
         </div>
     )
 }
 
 export default Song;
-// artist, album, time, is_favorite
