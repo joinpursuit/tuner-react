e85d2774a980a18b634a3d29f44db66b4b9ab24c
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-07 18:05:42 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {Routes, Route } from 'react-router-dom'
+
+//Pages
+import Edit from './Pages/Edit'
+import Home from './Pages/Home'
+import New from './Pages/New'
+import Index from './Pages/Index'
+import Show from './Pages/Show'
+import Error from './Pages/Error'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact to="/" element={<Home />} />
+        <Route to="/songs" element={<Index />} />
+        <Route to="/songs/:id" element={<Show />} />
+        <Route to="/songs/new" element={<New />} />
+        <Route to="/songs/:id/Edit" element={<Edit />} />
+        <Route to="*" element={<Error />} />
+      </Routes>
     </div>
   );
 }
