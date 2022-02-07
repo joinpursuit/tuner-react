3498728afda3a923ef6ff170cfdf2a1ca609bdaf
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-07 18:36:17 -0500

    added templates to pages

---
+++ src/Pages/Edit.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Edit() {
+  return (
+    <div className='Edit'>
+        Edit
+    </div>);
+}
+
+export default Edit;
---
+++ src/Pages/Index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Index() {
+  return (
+    <div>
+        Index
+    </div>);
+}
+
+export default Index;
