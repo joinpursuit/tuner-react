66aac55403da25cdee8362ecb59490f10931594c
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-07 19:17:41 -0500

    added Navbar w/basic css

---
+++ src/Components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Nav.css';
+import { Link } from 'react-router-dom';
+
+
+
+
+const Nav = () => {
+    return (
+        <div className='Nav'>
+                <Link to="/songs">
+                    <h1 className=''>Tuner</h1>
+                </Link>
+                <Link to="/songs/new">
+                    <button>
+                        New song
+                    </button>
+                </Link>
+        </div>
+    );
+};
+
+export default Nav;
