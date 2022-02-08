1db65bac120c2e9eee1dea68f7f28ee8a388beb1
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-08 11:43:52 -0500

    functioning show page

--- src/Components/DisplaySong.js
+++ src/Components/DisplaySong.js
@@ -9,10 +9,38 @@ const DisplaySong = () => {
     const URL = process.env.REACT_APP_API_URL;
     const navigate = useNavigate();
     
+    useEffect(()=>{
+        const getSong = async () => {
+            const targetSong = await axios.get(`${URL}/songs/${id}`)
+            setSong(targetSong.data)
+        }
+        getSong();
 
+    },[URL, id])
+
+    const { name, artist, album, time, is_favorite } = song;
+
+    const handleDelete = async () => {
+        await axios.delete(`${URL}/songs/${id}`)
+        navigate("/songs")
+    }
+    
   return (
     <div className='DisplaySong'>
-        DisplaySong
+        <h1>Name: {name}</h1>
+        <h2>Artist: {artist}</h2>
+        <h2>Album: {album}</h2>
+        <h2>Time: {time}</h2>
+        <h2>Favorite? {is_favorite}</h2>
+        <div className='buttons'>
+            <Link to="/songs">
+                <button>Back</button>
+            </Link>
+            <Link to={`/songs/${id}/edit`}>
+                <button>Edit</button>
+            </Link>
+            <button onClick={handleDelete}>Delete</button>
+        </div>
     </div>)
 };
 
