ef235803825facaa8af466d58beb5cc545c85664
Rose Durandisse <rosedurandisse@pursuit.org>
2022-02-09 12:38:09 -0500

    Component rendering each song

---
+++ src/Components/SongsDetails.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+
+function SongDetails() {
+  //navigate to another page
+  const navigate = useNavigate();
+  //grab the url in order for it to be used in javascript
+  const API = process.env.REACT_APP_API_URL;
+  //use the tool params, to grab the id that will be listed in the url
+  const { id } = useParams();
+  //will render as an object even if the useState is an array
+  const [song, setSong] = useState([]);
+  const [song, setSong] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${API}/songs/${id}`)
+      .then((response) => setSong(response.data))
+      .catch((error) => console.warn(error));
+  }, [id]);
+
+  const handleDelete = () => {
+    axios.delete(`${API}/songs/${id}`).then(
+      () => {
+        navigate(`/songs`);
+      },
+      (error) => console.error(error)
+    );
+  };
+
+  console.log(song);
+  return (
+    <div>
+      <article className="SongInformation">
+        <p>Name:{song.name}</p>
+        <p>Artist: {song.artist}</p>
+        <p>Album: {song.album}</p>
+      </article>
+      <Link to={`/songs/${song.id}/edit`}>
+        <button>Update</button>
+      </Link>
+      <button onClick={handleDelete}>Delete</button>
+      <button>Back</button>
+    </div>
+  );
+}
+export default SongDetails;
