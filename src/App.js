b95a8921ecb72081f7cdd7d31ce6a9cedf6f6afb
Rose Durandisse <rosedurandisse@pursuit.org>
2022-02-08 22:10:53 -0500

    Updated the routing to the Nav, Songs, and Home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+// import NavBar from "./Components/NavBar";
+import { Route, Routes, Router } from "react-router-dom";
+
+//Pages
+import Home from "./Pages/Home";
+
+//Components
+import NavBar from "./Components/NavBar";
+import AllSongs from "./Components/AllSongs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/songs" element={<AllSongs />} />
+      </Routes>
     </div>
   );
 }
