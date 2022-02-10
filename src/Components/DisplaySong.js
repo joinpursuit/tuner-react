d2126ee699164bf35297147af305fc0779997732
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-09 20:39:24 -0500

    refactored w/Bootstrap

--- src/Components/DisplaySong.js
+++ src/Components/DisplaySong.js
@@ -1,47 +1,66 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
+import './DisplaySong';
 import axios from 'axios';
-
+import { Card } from 'react-bootstrap';
 
 const DisplaySong = () => {
-    const [song, setSong] = useState({});
-    const { id } = useParams();
-    const URL = process.env.REACT_APP_API_URL;
-    const navigate = useNavigate();
-    
-    useEffect(()=>{
-        const getSong = async () => {
-            const targetSong = await axios.get(`${URL}/songs/${id}`)
-            setSong(targetSong.data)
-        }
-        getSong();
+  const [song, setSong] = useState({});
+  const { id } = useParams();
+  const URL = process.env.REACT_APP_API_URL;
+  const navigate = useNavigate();
 
-    },[URL, id])
+  useEffect(() => {
+    const getSong = async () => {
+      const targetSong = await axios.get(`${URL}/songs/${id}`);
+      setSong(targetSong.data);
+    };
+    getSong();
+  }, []);
 
-    const { name, artist, album, time, is_favorite } = song;
+  const { name, artist, album, time, is_favorite } = song;
 
-    const handleDelete = async () => {
-        await axios.delete(`${URL}/songs/${id}`)
-        navigate("/songs")
-    }
-    
+  const handleDelete = async () => {
+    await axios.delete(`${URL}/songs/${id}`);
+    navigate('/songs');
+  };
+
   return (
-    <div className='DisplaySong'>
-        <h1>Name: {name}</h1>
-        <h2>Artist: {artist}</h2>
-        <h2>Album: {album}</h2>
-        <h2>Time: {time}</h2>
-        <h2>Favorite? {is_favorite}</h2>
-        <div className='buttons'>
-            <Link to="/songs">
-                <button>Back</button>
-            </Link>
-            <Link to={`/songs/${id}/edit`}>
-                <button>Edit</button>
-            </Link>
-            <button onClick={handleDelete}>Delete</button>
-        </div>
-    </div>)
+    <div className="DisplaySong">
+      <table className="table table-striped table-bordered table-dark">
+        <thead>
+          <tr>
+            <td>Name: {name}</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Artist: {artist}</td>
+          </tr>
+          <tr>
+            <td>Album: {album}</td>
+          </tr>
+          <tr>
+            <td>Time: {time}</td>
+          </tr>
+          <tr>
+            <td>Favorite: {is_favorite ? 'Yes' : 'No'}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="buttons mt-5">
+        <Link to="/songs">
+          <button className="btn btn-dark mx-3">Back</button>
+        </Link>
+        <Link to={`/songs/${id}/edit`}>
+          <button className="btn btn-dark mx-3">Edit</button>
+        </Link>
+        <button className="btn btn-dark mx-3" onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DisplaySong;
