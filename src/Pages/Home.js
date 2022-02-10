d774b53ff59f1994433a0bad9632322021d3e304
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-02-10 17:12:56 -0500

    test

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ const Home = () => {
     return (
         <div>
             <h1>TUNER</h1>
+            {/* test */}
             <p>Created by Stephanie</p>
             <p>Contributors: Sev</p>
             <p>Special thanks to my team Justin and Fatema</p>
