db58a7117a5e237e7f26239e22143435125e42a2
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-09 21:19:00 -0500

    updated new css

--- src/Pages/New.js
+++ src/Pages/New.js
@@ -4,6 +4,7 @@ import SongForm from "../Components/SongForm"
 function New() {
     return (
         <div className='New'>
+            <h1>New Song</h1>
             <SongForm />
         </div>
     );
