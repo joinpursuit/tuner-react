091c2e4130d97ff9867fd07b598f451806da11fc
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-09 20:49:54 -0500

    refactored w/Bootstrap

--- src/Components/SongTable.js
+++ src/Components/SongTable.js
@@ -1,39 +1,37 @@
 import React from 'react';
 import axios from 'axios';
-import { useState, useEffect } from "react"
-import Song from './Song'
+import { useState, useEffect } from 'react';
+import Song from './Song';
+import './SongTable.css';
 
-function SongTable() {
-const [songs, setSongs] = useState([])
-const URL = process.env.REACT_APP_API_URL
+const SongTable = () => {
+  const [songs, setSongs] = useState([]);
+  const URL = process.env.REACT_APP_API_URL;
 
-useEffect(()=>{
+  useEffect(() => {
     const getSongs = async () => {
-        const allSongs = await axios.get(`${URL}/songs`)
-        setSongs(allSongs.data)
-    }
+      const allSongs = await axios.get(`${URL}/songs`);
+      setSongs(allSongs.data);
+    };
     getSongs();
-},[])
+  }, [URL]);
 
-const tableRows = songs.map((e)=> <Song song={e}/>)
-
-    return (
-        <div className='SongTable'>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Name</td>
-                        <td>Artist</td>
-                        <td>Time</td>
-                        <td>Favorite</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tableRows}
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  const tableRows = songs.map((e, i) => <Song song={e} key={'song' + i} />);
+  return (
+    <div className="SongTable">
+      <table className="table table-striped table-bordered table-dark">
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Artist</td>
+            <td>Time</td>
+            <td>Favorite</td>
+          </tr>
+        </thead>
+        <tbody>{tableRows}</tbody>
+      </table>
+    </div>
+  );
+};
 
 export default SongTable;
