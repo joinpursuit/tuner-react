264a520265752f250d77a390212d68df4b08bd4c
Angie G <angieg@Angies-MBP.fios-router.home>
2022-02-12 15:14:14 -0500

    Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from './Pages/Home';
+import Form from './Pages/New';
+import Show from './Pages/Show';
+import Edit from './Pages/Edit';
+
+// Components
+import NavBar from './Components/NavBar';
+import Songs from './Components/Songs';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Angie Stacy</h1>
+    <div className='wrapper'>
+
+        <main>
+
+          <Routes>
+            <Route path='/' element={ <Home /> } />
+            <Route path='/songs' element={ <Song />} />
+            <Route path='/songs/new' element={ <Form />} />
+            <Route exact path='/songs/:index' element={ <Show />} />
+            <Route path='/songs/:index/edit' element={ <Edit />} />
+          </Routes>
+
+        </main>
     </div>
   );
-}
+};
 
 export default App;
