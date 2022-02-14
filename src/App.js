655d5ef02c4c352e8e2111f80855b6be5ed4a494
Angie G <angieg@Angies-MBP.fios-router.home>
2022-02-14 18:33:27 -0500

    Front-End Routes

--- src/App.js
+++ src/App.js
@@ -2,29 +2,30 @@ import { Routes, Route } from 'react-router-dom';
 
 // Pages
 import Home from './Pages/Home';
-import Form from './Pages/New';
+import NewForm from './Pages/New';
 import Show from './Pages/Show';
-import Edit from './Pages/Edit';
+// import Edit from './Pages/Edit';
+import Reload from './Pages/Reload'
 
 // Components
-import NavBar from './Components/NavBar';
-import Songs from './Components/Songs';
+import UI from './Components/UI';
+import Song from './Components/Song';
 
 function App() {
   return (
     <div className='wrapper'>
-
-        <main>
+          <UI />
 
           <Routes>
             <Route path='/' element={ <Home /> } />
             <Route path='/songs' element={ <Song />} />
-            <Route path='/songs/new' element={ <Form />} />
-            <Route exact path='/songs/:index' element={ <Show />} />
-            <Route path='/songs/:index/edit' element={ <Edit />} />
+            <Route path='/songs/new' element={ <NewForm />} />
+            <Route exact path='/songs/:id' element={ <Show />} />
+            {/* <Route path='/songs/:id/edit' element={ <Edit />} /> */}
+            <Route path='*' element={<Reload />} />
           </Routes>
+      {/* <Footer /> */}
 
-        </main>
     </div>
   );
 };
