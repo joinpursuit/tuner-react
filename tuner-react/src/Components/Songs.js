00d5864359e54ed16cf9d4a7de5dc2bf94c4d3f5
Lortiz528 <lortiztech@gmail.com>
2022-08-09 20:48:22 -0400

    index route displays my songs

--- tuner-react/src/Components/Songs.js
+++ tuner-react/src/Components/Songs.js
@@ -1,22 +1,33 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Songs() {
   const API = process.env.REACT_APP_API_URL;
   const [songs, setSongs] = useState([]);
+
   useEffect(() => {
     axios
       .get(`${API}/songs`)
       .then((response) => {
         setSongs(response.data);
-        console.log(songs);
+        // console.log(songs);
       })
       .catch((error) => console.warn('catch', error));
   }, [API, songs]);
 
+  const songList = songs.map((song, index) => {
+    return (
+      <li key={index}>
+        <Link to={`/songs/${index}`}>{song.name}</Link>
+      </li>
+    );
+  });
+
   return (
     <div>
       <h1>This is our songs Index</h1>
+      <ul>{songList}</ul>
     </div>
   );
 }
