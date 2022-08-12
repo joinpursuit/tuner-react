52222ab82450069a027872984834b4bb037fdf5f
TaryneLeach <taryneleach@gmail.com>
2022-08-12 00:00:43 -0400

    forogt a lot of stuff

--- tuner-react/src/App.js
+++ tuner-react/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import Songs from './Components/Songs';
+import SongDeets from './Components/SongDeets';
+import SongEdit from './Components/SongEdit';
+import SongNew from './Components/SongNew';
+import Home from './Components/Home';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
+			<NavBar />
+			<Routes>
+				<Route path='/' element={<Home />}></Route>
+				<Route path='/songs' element={<Songs />}></Route>
+				<Route path='/songs/:id' element={<SongDeets />}></Route>
+				<Route path='/songs/new' element={<SongNew />}></Route>
+				<Route path='/songs/:id/edit' element={<SongEdit />}></Route>
+			</Routes>
 		</div>
 	);
 }
